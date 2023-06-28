9f1e118c5fd66e0e438275549ecbb93e354dfecc
Shubhamsagar07 <shubhamsagarprasad@gmail.com>
2023-06-28 17:17:45 +0530

    test: added target=_blank

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="dark:bg-[--bg-dark] dark:text-white text-center p-2">
-      Project by <a href="https://github.com/rajkaranm"> Rajkaran</a> and <a href="https://github.com/Shubhamsagar07">Shubhamsagar</a>
+      Project by <a href="https://github.com/rajkaranm" target="_blank"> Rajkaran</a> and <a href="https://github.com/Shubhamsagar07" target="_blank">Shubhamsagar</a>
     </div>
   );
 }
