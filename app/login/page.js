1fe088c3f767646d97087381eaa3f57578b6bcfb
Shubhamsagar07 <shubhamsagarprasad@gmail.com>
2023-07-08 11:23:25 +0530

    test: Added Google login ui in register and login page

--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useThemeContext, useUserContext } from "../context/store";
 import supabase from "@/utils/supabase";
 import { useRouter } from "next/navigation";
-
+import Image from "next/image";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -35,31 +35,44 @@ export default function Login() {
   }, [theme]);
   return (
     <div className="h-[38rem] flex flex-row justify-center items-center dark:bg-[--bg-dark]">
-      <div className="w-96 h-96 shadow-3xl p-11 rounded-lg">
-        <h1 className="dark:text-white text-center text-2xl">Login</h1>
+      <div className="w-96 h-[32rem] shadow-3xl p-11 rounded-lg">
+        <h1 className="dark:text-white text-center py-6 text-2xl">Login</h1>
+        <br></br><br></br>
         <div className="w-full h-36 flex flex-col justify-evenly items-center">
+          <button
+            onClick={() => handleLogin()}
+            className="w-full rounded-2xl text-black p-2 dark:text-black bg-[--primary-color] dark:bg-white display: flex "
+          >
+            <div className=" pr-8 pl-6 align-left">
+           <Image className=" " src="/GoogleLogo.jpg" width={25} height={25} />
+           </div> Login with Google
+          </button><br></br>
+          <p className="dark:text-white py-2 text-center">OR</p>
           <input
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white"
+            className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white "
             placeholder="Email"
             type="text"
             name=""
             id=""
           />
+          <br></br><br></br>
           <input
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white"
+            className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white "
             placeholder="Password"
             type="text"
             name=""
             id=""
           />
+          <br></br>
         </div>
-        <p className="dark:text-white py-6 text-center">Forgot password?</p>
+        <br></br>
+        <p className="dark:text-white py-8 text-center">Forgot password?</p>
         <div className="">
           <button
             onClick={() => handleLogin()}
-            className="w-full rounded-2xl text-white p-2 dark:text-white bg-[--primary-color]"
+            className="w-full rounded-2xl text-white p-2 dark:text-white bg-[--primary-color] "
           >
             Submit
           </button>
--- app/register/page.js
+++ app/register/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useThemeContext } from "../context/store";
 import supabase from "@/utils/supabase";
-
+import Image from "next/image";
 
 export default function register() {
   
@@ -33,9 +33,16 @@ export default function register() {
 
     return(
         <div className="h-[38rem] flex flex-row justify-center items-center dark:bg-[--bg-dark]">
-      <div className="w-96 h-[26rem] shadow-3xl p-11 rounded-lg">
+      <div className="w-96 h-[30rem] shadow-3xl p-11 rounded-lg">
         <h1 className="dark:text-white text-center text-2xl">Sign in</h1>
+        <br></br><br></br>
         <div className="w-full h-52 flex flex-col justify-evenly items-center">
+        <button onClick={() => handleRegister()} className="w-full rounded-2xl text-white p-2 dark:text-black bg-[--primary-color] bg-white display: flex">
+           <div className=" pr-7 pl-5">
+            <Image src="/GoogleLogo.jpg" width={25} height={25}/> </div> Sign in with Google
+          </button>
+          <br></br>
+          <p className="dark:text-white py-2 text-center">OR</p>
           <input
             onChange={(e) => setEmail(e.target.value)}
             className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white"
@@ -44,6 +51,7 @@ export default function register() {
             name=""
             id=""
           />
+          <br></br>
           <input
             onChange={(e) => setPassword(e.target.value)}
             className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white"
@@ -52,6 +60,7 @@ export default function register() {
             name=""
             id=""
           />
+          <br></br>
           <input
             className="w-full text-white border-black border-b-2 outline-none dark:bg-[--bg-dark] dark:border-white"
             placeholder="Confirm Password"
@@ -60,7 +69,7 @@ export default function register() {
             id=""
           />
         </div>
-        <p className="dark:text-white py-4 text-center"><a href="/login">Already have an account?</a></p>
+        <p className="dark:text-white py-8 text-center"><a href="/login">Already have an account?</a></p>
         <div className="">
           <button onClick={() => handleRegister()} className="w-full rounded-2xl text-white p-2 dark:text-white bg-[--primary-color]">
             Submit
